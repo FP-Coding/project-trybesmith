56b0e47eac5de87cfe5ca2df482a2f6ca4350194
Felipe Pinto <felipe14062000@gmail.com>
2023-02-06 15:13:05 -0300

    feat: criação da pasta inteface

--- src/utils/createPlaceholders.ts
+++ src/utils/createPlaceholders.ts
@@ -1,7 +1,4 @@
-interface IProduct {
-  name: string,
-  amount: string
-}
+import IProduct from '../interfaces/products';
 
 const createPlaceholders = (newProduct: IProduct): [string, string[]] => {
   const placeholders: string = Object.keys(newProduct).map((_item: string) => '?').join(', ');
